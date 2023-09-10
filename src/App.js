e126d4356ef130068f843a4f7e977e00313be387
Imminent-Darkness <swilkins@eou.edu>
2023-09-10 18:40:10 -0500

    "Converting static app to pure HTML/CSS"

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function App() {
-  const value = '';
-  return <div>{value}</div>;
-}
-
-export default App;
--- src/resume.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './resume.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('resume'));
